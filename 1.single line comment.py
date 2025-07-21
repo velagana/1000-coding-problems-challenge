1f90a83c432c46e22fecd47027ffc9f5e21ce5d4
velagana <116190024+velagana@users.noreply.github.com>
2025-07-21 16:41:22 +0530

    Add files via upload

---
+++ 1.single line comment.py
@@ -0,0 +1,9 @@
+#This is single line comment
+print('hello world')
+#this is another single line comment
+print('goodbye world')
+#yet another single line comment
+print('helo again')
+#this is last single line comment
+print('goodbye again')
+#end of the file comment
