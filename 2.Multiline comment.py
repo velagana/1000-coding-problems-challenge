ae3cd7323a20e5c459c1a7c3b91bda0a8b990938
velagana <116190024+velagana@users.noreply.github.com>
2025-07-22 14:04:12 +0530

    Add files via upload

---
+++ 2.Multiline comment.py
@@ -0,0 +1,12 @@
+'''
+this is a multiline comment
+it can span multiple lines
+it is useful for providing detailing explanations
+'''
+print('Helo world')
+print('Good world')
+print('helo again')
+
+'''
+this is another multiline 
+'''
