165ea7740f5dbaddd6d5cdb4d73b110cedb6e7dc
velagana <116190024+velagana@users.noreply.github.com>
2025-07-24 11:59:20 +0530

    Add files via upload

---
+++ 39.Check if a number is divisible by both 3 and 7.py
@@ -0,0 +1,5 @@
+num = int(input("enter the value of num"))
+if num % 7 == 0 and num % 3 == 0:
+    print("The given number is divided by 7 and 3")
+else:
+    print("The given number is not divided by 7 and 3:")
