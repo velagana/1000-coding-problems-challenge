70e273b123d06a0ff4536f3ad35117c7448b6f7f
velagana <116190024+velagana@users.noreply.github.com>
2025-07-24 12:02:05 +0530

    Add files via upload

---
+++ 47.Check if a person is a senior citizen.py
@@ -0,0 +1,5 @@
+snr_ctzn_age = int(input("Enter the age "))
+if snr_ctzn_age >= 60:
+    print("The person belongs to senior citizen:")
+else:
+    print("This person not belongs to senior citizen:")
