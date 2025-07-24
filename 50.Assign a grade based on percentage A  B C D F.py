46b6789e50042d2aa2a7032960486068a1401b93
velagana <116190024+velagana@users.noreply.github.com>
2025-07-24 12:03:10 +0530

    Add files via upload

---
+++ 50.Assign a grade based on percentage A  B C D F.py
@@ -0,0 +1,14 @@
+percentage = int(input("Enter the percentage :"))
+if percentage in range  (90 - 100):
+    print("A grade")
+elif percentage in range  (80 -90):
+    print("B grade") 
+elif percentage in range  (70 - 80):
+    print("C grade") 
+elif percentage in range  (60 - 70):
+    print("D grade") 
+elif percentage in range  (50 - 60):
+    print("E grade")
+else:
+    print("F grade")
+   
