b51387b445b026bdfe6618d6f500bc8b838904e0
velagana <116190024+velagana@users.noreply.github.com>
2025-07-24 11:58:41 +0530

    Add files via upload

---
+++ 37.Find the largest of three numbers.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the value of num1: "))
+num2 = int(input("Enter the value of num2: "))
+num3 = int(input("Enter the value of num3: "))
+if num1 > num2:
+    print("num1 is greater:")
+elif num2>num3:
+    print("num2 is greater")
+else:
+    print("num3 is greater:")
