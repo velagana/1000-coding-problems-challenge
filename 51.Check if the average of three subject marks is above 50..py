17489473a8c48c708f6d8d2270cbcd255defe550
velagana <116190024+velagana@users.noreply.github.com>
2025-07-25 20:19:50 +0530

    Add files via upload

---
+++ 51.Check if the average of three subject marks is above 50..py
@@ -0,0 +1,9 @@
+sub1 =int(input("Enter the sub1 marks:"))
+sub2 =int(input("Enter the sub2 marks:"))
+sub3 =int(input("Enter the sub3 marks:"))
+s = sub1 + sub2 + sub3
+s1 = s/3
+if s1 > 50:
+    print("The average of three subjects is greater than 50:")
+else:
+    print("not greater than 50:")
