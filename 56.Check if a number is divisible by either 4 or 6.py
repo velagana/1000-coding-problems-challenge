56ba5e9a730fd2b044c86a849296e7533fcee3c3
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:44:07 +0530

    Add files via upload

---
+++ 56.Check if a number is divisible by either 4 or 6.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the value of number"))
+if num % 4 == 0 or num % 6 == 0:
+                                 print("The number is divisible number")
+else:
+                                 print("NOT")
