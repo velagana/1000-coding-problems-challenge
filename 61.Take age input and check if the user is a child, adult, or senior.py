499ad5552ebd302c1368481b5f23ef15525df3e2
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:47:00 +0530

    Add files via upload

---
+++ 61.Take age input and check if the user is a child, adult, or senior.py
@@ -0,0 +1,9 @@
+age = int(input("Enter the age"))
+if age > 60:
+    print("Senior citizen:")
+elif age > 21 :
+    print("Adult")
+elif age > 14 :
+    print("Teenger")
+else:
+    print("children")
