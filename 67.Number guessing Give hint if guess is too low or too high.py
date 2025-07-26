681b722efc117254b53020891f68eed51930bfff
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:49:28 +0530

    Add files via upload

---
+++ 67.Number guessing Give hint if guess is too low or too high.py
@@ -0,0 +1,10 @@
+user_input = int(input("Enter the number"))
+guessing_number = 20
+if user_input ==  20:
+   print("Correct guess")
+elif user_input > 50:
+    print("The guess is too high")
+else:
+    print("The guess is too low ")
+
+
