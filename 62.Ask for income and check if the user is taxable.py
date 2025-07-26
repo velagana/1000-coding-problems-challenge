e2752a973c165a0b93991fdc5945089be60b424a
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:47:38 +0530

    Add files via upload

---
+++ 62.Ask for income and check if the user is taxable.py
@@ -0,0 +1,5 @@
+user_input = int(input("Enter the income of the person: "))
+if user_input >= 25000:
+    print("This person is eligible for tax")
+else:
+    print("This person is not eligible for tax:")
