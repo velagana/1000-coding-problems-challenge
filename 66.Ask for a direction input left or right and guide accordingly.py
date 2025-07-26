7f767a221b901682b6e1b3f89da9fb72f8b4ba8a
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:49:07 +0530

    Add files via upload

---
+++ 66.Ask for a direction input left or right and guide accordingly.py
@@ -0,0 +1,9 @@
+direction = input("Entered the diection")
+a = "right"
+b  = "left"
+if direction == a:
+    print("Go right")
+elif direction == b:
+    print("Go left: ")
+else:
+    print("Wrong direction :")
