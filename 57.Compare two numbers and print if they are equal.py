3b8bac31951ba87683d8408b1e2cbf7903f0cd9a
velagana <116190024+velagana@users.noreply.github.com>
2025-07-26 07:44:38 +0530

    Add files via upload

---
+++ 57.Compare two numbers and print if they are equal.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the value of num1: "))
+num2 = int(input("Enter the value of num2: "))
+if num1 == num2 :
+                 print("The given two numbers are equal:")
+else:            print("The given numbers are not equal numbers:")
