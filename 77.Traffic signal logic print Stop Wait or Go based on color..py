33157a3220fe984c9663cd1f7be0331c8ab789cb
velagana <116190024+velagana@users.noreply.github.com>
2025-07-27 14:07:59 +0530

    Add files via upload

---
+++ 77.Traffic signal logic print Stop Wait or Go based on color..py
@@ -0,0 +1,9 @@
+colour = input("Enter the trafic signal colour")
+if colour == "red":
+    print("Stop")
+elif colour =="Yellow":
+    print("Wait")
+elif colour =="Green":
+    print("GO")
+else:
+    print("Not a valid colour")
