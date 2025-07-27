a07fee8e978f39bf174c88233df462e30fa69ef1
velagana <116190024+velagana@users.noreply.github.com>
2025-07-27 14:06:30 +0530

    Add files via upload

---
+++ 72.Check if a user has enough balance to withdraw money.py
@@ -0,0 +1,6 @@
+req_bal = 500
+drw_amnt = int(input("Enter the mount want to draw: "))
+if drw_amnt < req_bal :
+    print("Insuffient funds or dont have enough balance: ")
+else:
+    print("money suceessfully drawn: ")
