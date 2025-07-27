05020f3e4605c67e86fa31c2f86808c448199229
velagana <116190024+velagana@users.noreply.github.com>
2025-07-27 14:07:23 +0530

    Add files via upload

---
+++ 75.Check if user qualifies for a discount purchase 1000.py
@@ -0,0 +1,6 @@
+amnt_purchased = int(input("Enter amount you puechased:"))
+discount_amount = 1000
+if amnt_purchased > discount_amount:
+    print("qulified for discount:")
+else:
+    print("Not qualified for discount:")
