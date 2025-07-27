fe97fa6d62d8fa672891edb70118c9de99ffcacf
velagana <116190024+velagana@users.noreply.github.com>
2025-07-27 14:08:58 +0530

    Add files via upload

---
+++ 81.Mobile recharge Check if balance is enough for selected plan.py
@@ -0,0 +1,9 @@
+print("plan1 : 399rs - 2gb + unlimited calls")
+print("plan2 :299 rs - 1.5 gb+unlimited calls")
+plan = int(input("Enter the plan:"))
+if plan == 399:
+    print("Plan A")
+elif plan == 299:
+    print("Pan B")
+else:
+    print("Not a valid plan")
